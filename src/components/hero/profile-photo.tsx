6c1e825adab33c9b9337ffa6147f887201dc54ba
Renke Brixel <renke.brixel@gmail.com>
2025-12-03 11:38:38 +0100

    adjust sorting

--- src/components/hero/profile-photo.tsx
+++ src/components/hero/profile-photo.tsx
@@ -49,7 +49,7 @@ export function ProfilePhoto() {
         }}
       >
         {/* Card container with subtle border */}
-        <div className='relative w-fit rounded-lg border border-border/30 bg-muted/50 p-0.5'>
+        <div className='border-border/30 bg-muted/50 relative w-fit rounded-lg border p-0.5'>
           {/* Front face - the image */}
           <img
             src={profileImage}
